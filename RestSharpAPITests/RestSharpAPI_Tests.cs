3b9141287936fd4f69b4c5f5932c41963e30ffc9
Aleksandar Bakardzhiev <compworld@abv.bg>
2023-02-19 11:31:33 +0200

    Update RestSharpAPI_Tests.cs

--- RestSharpAPITests/RestSharpAPI_Tests.cs
+++ RestSharpAPITests/RestSharpAPI_Tests.cs
@@ -2,6 +2,29 @@ using RestSharp;
 using System.Net;
 using System.Text.Json;
 
+//API Endpoints
+//TaskBoard exposes a RESTful API, available at:
+//https://taskboardjs.alexlb1.repl.co/api or in your case http://{yoursite}/api
+//The following endpoints are supported:
+//•	GET / api – list all API endpoints
+//•	GET /api/tasks – list all tasks (returns JSON array of tasks)
+//•	GET / api / tasks / id – returns a task by given id
+//•	GET /api/tasks/search/keyword – list all tasks matching given keyword
+//•	GET /api/tasks/board/boardName – list tasks by board name
+//•	POST /api/tasks – create a new task(post a JSON object in the request body, e.g. {"title":"Add Tests", "description":"API + UI tests", "board":"Open"})
+//•	PATCH / api / tasks / id – edit task by id (send a JSON object in the request body, holding the fields to modify, e.g. {"title":"changed title", "board":"Done"})
+//•	DELETE / api / tasks / id – delete task by id
+//•	GET /api/boards – list all boards
+
+//RESTful API: RestSharp API Tests
+//Your task is to write automated tests in C# for certain RESTful API endpoints. You should implement the following automated tests (35 points):
+//•	List the tasks and assert that the first task from board "Done" has title "Project skeleton" (8 points).
+//•	Find tasks by keyword "home" and assert that the first result has title "Home page" (5 points).
+//•	Find tasks by keyword "missing{randnum}" and assert that the results are empty (5 points).
+//•	Try to create a new task, holding invalid data, and assert an error is returned (5 points).
+//•	Create a new task, holding valid data, and assert the new task is successfully created with respective properties (12 points).
+
+
 namespace RestSharpAPITests
 {
     public class RestSharpAPI_Tests
